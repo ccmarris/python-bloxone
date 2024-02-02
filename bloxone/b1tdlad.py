04fd5bc265736eafbb4da7df46daa0f7d926ee53
Chris Marrison <chris@infoblox.com>
2024-02-02 08:49:20 -0800

    20240201 Updates to b1tdlad class

--- bloxone/b1tdlad.py
+++ bloxone/b1tdlad.py
@@ -41,16 +41,22 @@
 
 ------------------------------------------------------------------------
 '''
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 __author__ = 'Chris Marrison/Krishna Vasudeva'
 __author_email__ = 'chris@infoblox.com'
 
 import bloxone
 import logging
+import csv
+import sys
+try:
+    from rich import print
+except ImportError:
+    logging.debug('Failed to import print from rich module.')
 
 # ** Global Vars **
 
-class b1tdlad(bloxone.b1):
+class b1tdlad(bloxone.b1td):
 
     # Generic Methods
     def get(self, objpath, **params):
@@ -60,7 +66,6 @@ class b1tdlad(bloxone.b1):
         Parameters:
             objpath (str):  Swagger object path
             id (str):       Optional Object ID
-            action (str):   Optional object action, e.g. "nextavailableip"
         
         Returns:
             response object: Requests response object
@@ -97,22 +102,106 @@ class b1tdlad(bloxone.b1):
         return response
     
 
-    def get_lookalikes(self, target_domain='', format='json', **params):
+    def get_lookalikes(self, target_domain='', **params):
         '''
         '''
-        result = None
-
         url = f'{self.tdlad_url}/lookalikes'
+
         if target_domain:
-            filter = f'targetdomain=="{target_domain}"'
+            filter = f'target_domain=="{target_domain}"'
             url = self._add_params(url, _filter=filter)
             url = self._add_params(url, first_param=False, **params)
         else:
             url = self._add_params(url, **params)
         
         response = self._apiget(url)
-        if response.status_code in self.return_codes_ok:
-            result = response.json()
         
-        return result
+        return response
+    
 
+    def lookalike_to_csv(self, records, file=sys.stdout, enriched=False):
+        '''
+        Output lookalike data to CSV
+
+        Parameters:
+            records (list): Lookalike API result data
+            file (filehandler): File opened for write with newline=''
+            enriched (bool): Enhance data using dossier
+        '''
+        lookalikes = records.get('results')
+        if not lookalikes:
+            lookalikes = records
+        
+        if lookalikes:
+            # Check whether this is enriched output
+            if enriched:
+                headers = ['target_domain', 'lookalike_domain', 
+                           'registration_date', 'phishing', 'malware', 
+                           'suspicious', 'categories', 'reason', 'registrar',
+                           'last_updated', 'country', 'region',
+                           'cert_issued_by', 'cert_expiry' ]
+                w = csv.DictWriter(file, fieldnames=headers, extrasaction='ignore')
+                w.writeheader()
+                for l in lookalikes:
+                    # Enrich lookalike 
+                    r = self.enrich_lookalike(l)
+                    w.writerow(l)
+
+            else:
+                # Simple Output
+                headers = ['target_domain', 'lookalike_domain', 'registration_date', 
+                        'phishing', 'malware', 'suspicious', 'categories', 'reason']
+
+                try:
+                    w = csv.DictWriter(file, fieldnames=headers, extrasaction='ignore')
+                    w.writeheader()
+                    w.writerows(lookalikes)
+                except Exception as err:
+                    logging.error(f'Error occured writing CSV: {err}')
+                    raise
+        else:
+            logging.error('No data found')
+
+        return
+        
+
+    def enrich_lookalike(self, record):
+        '''
+        '''
+        sources = [ "geo", "whois", "ssl_cert" ]
+        host = record.get('lookalike_domain')
+
+        if host:
+            response = self.dossierquery(host, type='host', sources=sources )
+            if response.status_code in self.return_codes_ok:
+                results = response.json().get('results')
+                if results:
+                    for r in results:
+                        src = r.get('params').get('source')
+                        data = r.get('data')
+                        if src == 'whois':
+                            pw = data.get('response').get('parsed_whois')
+                            registrar = pw.get('registrar').get('name')
+                            udate = pw.get('updated_date')
+                        elif src == 'geo':
+                            country = data.get('country_name')
+                            region = data.get('region')
+                        elif src == "ssl_cert":
+                            ci = data.get('issuer')
+                            cert_exp = data.get('ssl_cert_chain')[0].get('expires')
+
+                    # Update record
+                    enrichment = { 'registrar': registrar,
+                                   'last_updated': udate,
+                                   'country': country,
+                                   'region': region,
+                                   'cert_issued_by': ci,
+                                   'cert_expiry': cert_exp }
+                    
+                    record.update(enrichment)
+            else:
+                logging.error(f'Could not enrich, API error: {response.status_code}')
+        else:
+            logging.error('No lookalike found in record')
+
+        return record
